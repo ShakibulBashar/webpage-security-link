cf2b6b55b8d39dfa1e5d4e34b473ddf35ccb7858
Shakibul Bashar <sakibbashar3@gmail.com>
2026-07-09 07:11:27 +0600

    Remove dist artifacts from tracking

--- dist/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"aeg8a6YcKzVnF8yxDcfGaDP6Mrooo/MORrJgPBI0ZtY=\"\n}"
--- dist/dev/static/chunks/app_layout_007e4b2.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__042hf1-._.css","static/chunks/node_modules_next_dist_1nyev3z._.js","static/chunks/app_components_1t2lgjc._.js"],
-    source: "dynamic"
-});
