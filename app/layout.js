9e9ad1f862adfbcfb5932d7da6b57dadd46ec8a7
Shakibul Bashar <sakibbashar3@gmail.com>
2026-07-24 00:22:52 +0600

    done

--- app/layout.js
+++ app/layout.js
@@ -117,7 +117,7 @@ export default function RootLayout({ children }) {
       <head>
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block"
         />
         <script
           type="application/ld+json"
